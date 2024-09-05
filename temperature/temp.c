459163c402283b42c864371e03591b81b1e6e255
dzerzzz <jaroprz123@gmail.com>
2024-09-05 20:39:50 +0200

    added temperature data collection every 60s

--- temperature/temp.c
+++ temperature/temp.c
@@ -12,99 +12,107 @@
 const char *fifo_path = "/tmp/uart_fifo";
 int fifo_fd;
 
-void sigint_handler (int signal) {
+void sigint_handler(int signal) {
     printf("\nTerminating program...\n");
     close(fifo_fd);
     _exit(0);
 }
 
-int main() 
-{
-	signal(SIGINT, sigint_handler);
+int main() {
+    signal(SIGINT, sigint_handler);
 
-	// Create I2C bus
-	int file;
-	char *bus = "/dev/i2c-1";
-	if ((file = open(bus, O_RDWR)) < 0) 
-	{
-		printf("Failed to open the bus. \n");
-		exit(1);
-	}
-	// Get I2C device, MCP9808 I2C address is 0x18(24)
-	ioctl(file, I2C_SLAVE, 0x18);
+    // Create I2C bus
+    int file;
+    char *bus = "/dev/i2c-1";
+    if ((file = open(bus, O_RDWR)) < 0) {
+        printf("Failed to open the bus.\n");
+        exit(1);
+    }
 
-	// Select configuration register(0x01)
-	// Continuous conversion mode, Power-up default(0x00, 0x00)
-	char config[3] = {0};
-	config[0] = 0x01;
-	config[1] = 0x00;
-	config[2] = 0x00;
-	write(file, config, 3);
-	// Select resolution rgister(0x08)
-	// Resolution = +0.0625 / C(0x03)
-	config[0] = 0x08;
-	config[1] = 0x02;
-	write(file, config, 2);
-	sleep(1);
+    // Get I2C device, MCP9808 I2C address is 0x18(24)
+    ioctl(file, I2C_SLAVE, 0x18);
 
-	// Read 2 bytes of data from register(0x05)
-	// temp msb, temp lsb
-	char reg[1] = {0x05};
-	write(file, reg, 1);
-	char data[2] = {0};
+    // Select configuration register(0x01)
+    // Continuous conversion mode, Power-up default(0x00, 0x00)
+    char config[3] = {0};
+    config[0] = 0x01;
+    config[1] = 0x00;
+    config[2] = 0x00;
+    write(file, config, 3);
 
-	time_t now = time(NULL);
-	struct tm* local = localtime(&now);
-	char filename[30];
-	strftime(filename, sizeof(filename), "TMP_%d-%H-%M.txt", local);
+    // Select resolution register(0x08)
+    // Resolution = +0.0625 / C(0x03)
+    config[0] = 0x08;
+    config[1] = 0x02;
+    write(file, config, 2);
+    sleep(1);
 
-	FILE *outputFile;
-	outputFile = fopen(filename, "w");
-	if(outputFile == NULL){
-		printf("Error opening file.\n");
-		return 1;
-	}
+    // Read 2 bytes of data from register(0x05)
+    // temp msb, temp lsb
+    char reg[1] = {0x05};
+    write(file, reg, 1);
+    char data[2] = {0};
 
-	// Open FIFO buffer of UART handler 
-	   if (access(fifo_path, F_OK) != 0) {
+    // Open FIFO buffer of UART handler
+    if (access(fifo_path, F_OK) != 0) {
         printf("[ERROR] uart_handler is down, exiting\n");
         return -1;
     }
     mkfifo(fifo_path, 0666);
-	
+    
     fifo_fd = open(fifo_path, O_WRONLY);
-	uint8_t frame_buffer[FRAME_SIZE];
-	frame out;
+    uint8_t frame_buffer[FRAME_SIZE];
+    frame out;
     out.command = SENSOR_READING;
 
-	while(1) {
-		if(read(file, data, 2) != 2)
-		{
-			printf("Error : Input/Output error \n");
-			return 1;
-		}
-		else
-		{
-			// Convert the data to 13-bits
-			int temp = ((data[0] & 0x1F) * 256 + data[1]);
-			if(temp > 4095)
-			{
-				temp -= 8192;
-			}
-			float cTemp = temp * 0.0625;
+    time_t start_time = time(NULL);
+    FILE *outputFile = NULL;
 
-			// Output data to screen
-			printf("Temperature in Celsius is : %.2f C \n", cTemp);
-			time_t ts = time(NULL);
-			printf("test %d %.2f\n", ts, cTemp);
-			fprintf(outputFile, "%d %.2f\n", ts, cTemp); 
+    while (1) {
+        time_t current_time = time(NULL);
+        if (current_time - start_time >= 60 || outputFile == NULL) {
+            if (outputFile != NULL) {
+                fclose(outputFile);
+            }
+            struct tm* local = localtime(&current_time);
+            char filename[30];
+            strftime(filename, sizeof(filename), "TMP_%d-%H-%M.txt", local);
+            outputFile = fopen(filename, "w");
+            if (outputFile == NULL) {
+                printf("Error opening file.\n");
+                return 1;
+            }
+            start_time = current_time;
+        }
+
+        if (read(file, data, 2) != 2) {
+            printf("Error: Input/Output error\n");
+            return 1;
+        } else {
+            int temp = ((data[0] & 0x1F) * 256 + data[1]);
+            if (temp > 4095) {
+                temp -= 8192;
+            }
+            float cTemp = temp * 0.0625;
+
+            // printf("Temperature in Celsius is: %.2f C\n", cTemp);
+            fprintf(outputFile, "%ld %.2f\n", current_time, cTemp);
 
 			// Send to UART handler
-			out.value = cTemp;
-    		encode(frame_buffer, &out);
-			write(fifo_fd, frame_buffer, FRAME_SIZE);
-		}
-		sleep(1);
-		fflush(outputFile);	
-	}
+            out.value = cTemp;
+            encode(frame_buffer, &out);
+            write(fifo_fd, frame_buffer, FRAME_SIZE);
+        }
+
+        fflush(outputFile);
+
+        sleep(1);
+    }
+
+    if (outputFile != NULL) {
+        fclose(outputFile);
+    }
+
+    close(file);
+    return 0;
 }
